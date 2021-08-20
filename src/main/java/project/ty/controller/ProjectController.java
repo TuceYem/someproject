00bfbe01073c377980db821e5108948f27712323
tuce_ <tuce_yemisen@hotmail.de>
2021-08-20 17:11:41 +0200

    tutorial implementation

---
+++ src/main/java/project/ty/controller/ProjectController.java
@@ -0,0 +1,33 @@
+package project.ty.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProjectController {
+
+//    @Autowired
+//    EmployeeService projectService;
+
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        return "Hello World";
+    }
+
+//    @GetMapping("/Employee")
+//    public List<Employee> allEmployees(){
+//        return this.projectService.getAllEmployees();
+//    }
+//
+//    @PostMapping("/Employee")
+//    public Employee addEmployee(@RequestBody Employee employee){
+//        return this.projectService.saveEmployee(employee);
+//    }
+//
+//    @DeleteMapping("/Employee")
+//    public void employeeDeleted (@RequestParam("id") Long id){
+//        this.projectService.deleteEmployee(id);
+//    }
+}
