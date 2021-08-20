23c84a63cd4a2a446566a7ea12576efaa9358d69
tuce_ <tuce_yemisen@hotmail.de>
2021-08-20 17:57:30 +0200

    postgres deleted + h2 adjustments

--- src/main/java/project/ty/TyApplication.java
+++ src/main/java/project/ty/TyApplication.java
@@ -2,8 +2,16 @@ package project.ty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan("project.ty.repo")
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 public class TyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/project/ty/repo/EmployeeRepository.java
+++ src/main/java/project/ty/repo/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package project.ty.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.ty.model.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
